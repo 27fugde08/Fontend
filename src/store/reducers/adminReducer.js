5ec304513f12f21ed8fa9ca1782ff471e18c7dca
27fudge08 <nguyentrongduy27082002@gmail.com>
2023-10-30 12:43:12 +0700

    30/10/2023 59

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -1,25 +1,51 @@
 import actionTypes from '../actions/actionTypes';
 
 const initialState = {
+    isLoadingGender: false,
     genders: [],
     roles: [],
-    position: []
+    positions: []
 }
 
 const adminReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.FETCH_GENDER_START:
-            console.log('fire fetch gender start: ', action)
+            let copyState = { ...state };
+            copyState.isLoadingGender = true;
             return {
-                ...state,
+                ...copyState,
             }
         case actionTypes.FETCH_GENDER_SUCCESS:
-            console.log('fire fetch gender success: ', action)
+            state.genders = action.data;
+            state.isLoadingGender = false;
             return {
                 ...state,
             }
         case actionTypes.FETCH_GENDER_FAILED:
-            console.log('fire fetch gender failed: ', action)
+            state.isLoadingGender = false;
+            state.genders = [];
+            return {
+                ...state,
+            }
+
+        case actionTypes.FETCH_POSITION_SUCCESS:
+            state.positions = action.data;
+            return {
+                ...state,
+            }
+        case actionTypes.FETCH_POSITON_FAILED:
+            state.positions = [];
+            return {
+                ...state,
+            }
+
+        case actionTypes.FETCH_ROLE_SUCCESS:
+            state.roles = action.data;
+            return {
+                ...state,
+            }
+        case actionTypes.FETCH_ROLE_FAILED:
+            state.roles = [];
             return {
                 ...state,
             }
