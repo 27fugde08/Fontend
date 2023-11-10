4d29d64815fd6f2584e512663a281eaee9d363ad
27fudge08 <nguyentrongduy27082002@gmail.com>
2023-11-11 03:20:38 +0700

    done 78

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -19,13 +19,8 @@ export const adminMenu = [
             //     name: 'menu.admin.manage-admin', link: '/system/user-admin'
             // },
             { //quản lí khám bệnh của bác sĩ
-                name: 'menu.doctor.manage-schedule',
-                menus: [
-                    {
-                        name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
-                    },
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
 
-                ]
             },
         ]
     },
